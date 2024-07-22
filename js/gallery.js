16cb76d6c1cf45ea1fc589b0363c2a9a84f8870a
Kostia <kostiakolesnik2513@gmail.com>
2024-07-22 18:56:07 +0200

    create gallery page and add a little animation

---
+++ js/gallery.js
@@ -0,0 +1,51 @@
+const photos = [
+  { name: "Image(1)" },
+  { name: "Image(2)" },
+  { name: "Image(3)" },
+  { name: "Image(4)" },
+  { name: "Image(5)" },
+  { name: "Image(6)" },
+  { name: "Image(7)" },
+  { name: "Image(8)" },
+  { name: "Image(9)" },
+  { name: "Image(10)" },
+  { name: "Image(11)" },
+  { name: "Image(12)" },
+  { name: "Image(13)" },
+  { name: "Image(14)" },
+  { name: "Image(15)" },
+  { name: "Image(16)" },
+  { name: "Image(17)" },
+  { name: "Image(18)" },
+  { name: "Image(19)" },
+  { name: "Image(20)" },
+  { name: "Image(21)" },
+  { name: "Image(22)" },
+  { name: "Image(23)" },
+  { name: "Image(24)" },
+  { name: "Image(25)" },
+  { name: "Image(26)" },
+  { name: "Image(27)" },
+  { name: "Image(28)" },
+  { name: "Image(29)" },
+  { name: "Image(30)" },
+];
+
+const listForItems = document.getElementById("gallery");
+// console.log(listForItems);
+
+const photoItem = (photos) => {
+  const ulList = document.createElement("ul");
+  ulList.classList.add("gallery__list");
+  photos.forEach((photo) => {
+    const item = document.createElement("li");
+    item.classList.add("gallery__item");
+    const img = document.createElement("img");
+    img.src = `./../img/Gallery/${photo.name}.jpg`;
+    item.append(img);
+    ulList.append(item);
+  });
+  return ulList;
+};
+
+listForItems.append(photoItem(photos));
--- js/index.js
+++ js/index.js
@@ -1,49 +1,12 @@
-const photos = [
-  { name: "Image(1)", width: 100, height: 130 },
-  { name: "Image(2)", width: 100, height: 130 },
-  { name: "Image(3)", width: 100, height: 130 },
-  { name: "Image(4)", width: 100, height: 130 },
-  { name: "Image(5)", width: 100, height: 130 },
-  { name: "Image(6)", width: 100, height: 130 },
-  { name: "Image(7)", width: 100, height: 130 },
-  { name: "Image(8)", width: 100, height: 130 },
-  { name: "Image(9)", width: 100, height: 130 },
-  { name: "Image(10)", width: 100, height: 130 },
-  { name: "Image(11)", width: 100, height: 130 },
-  { name: "Image(12)", width: 100, height: 130 },
-  { name: "Image(13)", width: 100, height: 130 },
-  { name: "Image(14)", width: 100, height: 130 },
-  { name: "Image(15)", width: 100, height: 130 },
-  { name: "Image(16)", width: 100, height: 130 },
-  { name: "Image(17)", width: 100, height: 130 },
-  { name: "Image(18)", width: 100, height: 130 },
-  { name: "Image(19)", width: 100, height: 130 },
-  { name: "Image(20)", width: 100, height: 130 },
-  { name: "Image(21)", width: 100, height: 130 },
-  { name: "Image(22)", width: 100, height: 130 },
-  { name: "Image(23)", width: 100, height: 130 },
-  { name: "Image(24)", width: 100, height: 130 },
-  { name: "Image(25)", width: 100, height: 130 },
-  { name: "Image(26)", width: 100, height: 130 },
-  { name: "Image(27)", width: 100, height: 130 },
-];
-
-const listForItems = document.querySelector(".body");
-
-const photoItem = (photos) => {
-  const ulList = document.createElement("ul");
-  ulList.classList.add("gallery__list");
-  photos.forEach((photo) => {
-    const item = document.createElement("li");
-    item.classList.add("gallery__item");
-    const img = document.createElement("img");
-    img.src = `./srs/img/Gallery/${photo.name}.jpeg`;
-    img.style.width = `${photo.width}px`;
-    img.style.height = `${photo.height}px`;
-    item.append(img);
-    ulList.append(item);
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollEl = document.getElementById("scrollBtn");
+  scrollEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    let target = scrollEl.getAttribute("href");
+    let targetPosition = document.querySelector(target).offsetTop;
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth",
+    });
   });
-  return ulList;
-};
-
-listForItems.append(photoItem(photos));
+});
